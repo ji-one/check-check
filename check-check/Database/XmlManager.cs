c5cbf8aa7a0c5d463eff3e7ac5cdee4c97a88814
ji-one <ds020228@naver.com>
2022-04-20 12:10:19 +0900

    feat: create a XmlManager to read XML file

---
+++ check-check/Database/XmlManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace check_check.Database
+{
+    public class XmlManager
+    {
+        private static string configFile = @"../../Config.xml";
+        public static string ConfigFile { get; set; }
+
+
+        public static string GetValue(params string[] args)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                XDocument xDoc = XDocument.Load(configFile);
+                result = GetNodeValue(xDoc.FirstNode as XElement, 0, args);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return result;
+        }
+
+        private static string GetNodeValue(XElement node, int idx, params string[] args)
+        {
+            string result = args.Length > idx + 1 ? GetNodeValue(node.Element(args[idx]), ++idx, args) : node.Element(args[idx]).Value.ToString();
+            return result;
+        }
+    }
+}
